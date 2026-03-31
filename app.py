4c97605be68a96df2beeaa6c665d1dbef8dc1620
ahmed alhamad <7ahmed.alhamad7@gmail.com>
2026-03-31 19:50:05 +0300

    creation of index.html file

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from datetime import datetime
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 @app.route('/api/incidents', methods=['GET'])
 def list_incidents():
     db = get_db()
